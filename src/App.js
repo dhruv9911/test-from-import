6fe06f65fba4f7e31e102686f20d6fddbd2f26e5
dhruv9911 <84926981+dhruv9911@users.noreply.github.com>
2021-09-28 00:21:11 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React By Dhruv Singh
+          Learn React By Dhruv Singh Bhadauriya
         </a>
       </header>
     </div>
